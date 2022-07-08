ca3ea091f9c013a2d0000230589c0882566b5332
RAUNAK BANJARI <88834316+raunakbanjari@users.noreply.github.com>
2022-07-08 18:35:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-img = ""
+img = "";
+stats = "";
+
 
 function setup(){
     canvas = createCanvas(800,450);
     canvas.center();
     background("white");
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : detecting objects"; 
+
 }
 function preload(){
    img = loadImage("dog_cat.jpg");
@@ -16,5 +21,25 @@ function draw(){
     stroke("red");
     noFill();
     rect(70 , 40 , 350 , 370);
+
+    fill("red");
+    text("CAT" , 390 , 100);
+    stroke("red");
+    noFill();
+    rect(330 , 70 , 400 , 370);
+}
+
+function modelLoaded(){
+    console.log("model is loaded");
+    stats = true;
+    objectDetector.detect(img , gotResults);
+}
+function gotResults(error , results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
 
 }
