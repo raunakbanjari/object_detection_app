52ca1448c6c01d01c7b186ba4bca4c213211cd79
RAUNAK BANJARI <88834316+raunakbanjari@users.noreply.github.com>
2022-07-15 18:43:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,29 +4,35 @@ objects = [];
 
 
 function setup(){
-    canvas = createCanvas(800,450);
+    canvas = createCanvas(380,380);
     canvas.center();
-    background("white");
-    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
-    document.getElementById("status").innerHTML = "Status : detecting objects"; 
+    
+    video = createCapture(VIDEO);
+    video.size(380 , 380);
+    video.hide();
+    
 
-}
-function preload(){
-   img = loadImage("dog_cat.jpg");
 }
 
 function draw(){
-    image(img , 0 , 0 , 800 , 450);
+    image(video , 0 , 0 , 380 , 380);
 
     if(stats != ""){
+        objectDetector.detect(video , gotResults);
+
+        r = random(255);
+        g = random(255);
+        b = random(255);
+
         for (i = 0; i < objects.length; i++){
            document.getElementById("status").innerHTML = "Objects detected";
+           document.getElementById("no_of_objects").innerHTML = "Number of objects detected: " + objects.length;
            
-           fill("red");
+           fill(r ,g , b);
            percent = floor(objects[i].confidence * 100);
            text(objects[i].label + " " + percent + "%" , objects[i].x , objects[i].y);
            noFill();
-           stroke("red");
+           stroke(r , g , b);
            rect(objects[i].x ,  objects[i].y ,  objects[i].width ,  objects[i].height);
         }
     }
@@ -34,11 +40,15 @@ function draw(){
    
 
 }
+function start(){
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : detecting objects"; 
+}
 
 function modelLoaded(){
     console.log("model is loaded");
     stats = true;
-    objectDetector.detect(img , gotResults);
+    
 }
 function gotResults(error , results){
     if(error){
